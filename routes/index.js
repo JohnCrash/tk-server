52977df3cd7aba3464ad1268936019d99a57a70e
john <udsn2@sina.com>
2017-04-27 17:58:42 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const config = {
   database:'ep_tiku',
 }
 
+const alltopicBOOK = '全部题库';
 /*
  * 返回书面列表
  * 无参数
@@ -20,15 +21,16 @@ const config = {
  */
 router.get('/book/', function (req, res) {
   new sql.ConnectionPool(config).connect().then(pool=>{
-    return pool.request().query('select distinct BookName from BookIndex')
+    return pool.request().query('select distinct BookName from BookIndex');
   }).then(result=>{
-    let ret = []
+    let ret = [];
+    ret.push({BookName:alltopicBOOK});
     for( let i=0;i<result.recordset.length;i++){
-      ret.push(result.recordset[i])
+      ret.push(result.recordset[i]);
     }
-    res.json(ret)
+    res.json(ret);
   }).catch(err=>{
-    res.send(err)
+    res.send(err);
   })
 });
 
@@ -54,35 +56,70 @@ router.get('/module/', function (req, res) {
   let Module = req.query['Module'];
   if(BookName){
     if(Module){
-      //search units
-      new sql.ConnectionPool(config).connect().then(pool=>{
-          return pool.request().query(`select id,BookLesson from BookIndex where BookName='${BookName}' and BookUnit='${Module}'`)
-        }).then(result=>{
-          let ret = []
-          for( let i=0;i<result.recordset.length;i++){
-            ret.push({BookIndex:result.recordset[i].id,
-              Unit:result.recordset[i].BookLesson})
+      if(BookName===alltopicBOOK){
+        //全部
+        let m = Module.match(/SECTION\s(\d+)-(\d+)/);
+        let ret = [];
+        if(m && m[1] && m[2]){
+          
+          for( let i=Number(m[1]);i<Number(m[2]);i+=10 ){
+            ret.push({
+              Unit:`UNIT ${i}-${i+10}`,
+              UnitBegin:m[1],
+              UnitEnd:m[2]
+            });
           }
-          res.json(ret)
-        }).catch(err=>{
-          res.send(err)
-        })
+        }
+        res.json(ret);
+      }else{
+        //search units
+        new sql.ConnectionPool(config).connect().then(pool=>{
+            return pool.request().query(`select id,BookLesson from BookIndex where BookName='${BookName}' and BookUnit='${Module}'`);
+          }).then(result=>{
+            let ret = [];
+            for( let i=0;i<result.recordset.length;i++){
+              ret.push({BookIndex:result.recordset[i].id,
+                Unit:result.recordset[i].BookLesson});
+            }
+            res.json(ret);
+          }).catch(err=>{
+            res.send(err);
+          })
+      }
+    }else if(BookName===alltopicBOOK){
+      //枚举全部题库数据，每个章中有10个节，每个节有10个题。每一章就有100题
+      new sql.ConnectionPool(config).connect().then(pool=>{
+        return pool.request().query(`SELECT max(rowid) as count FROM raw_db`);
+      }).then(result=>{
+        let count = result.recordset[0].count;
+        let i;
+        let ret = [];
+        for(i = 0;i<count;i+=100){
+          if(i+100<count)
+            ret.push({Module:`SECTION ${i}-${i+100}`});
+          else
+            ret.push({Module:`SECTION ${i}-${count}`});
+        }
+        res.json(ret);
+      }).catch(err=>{
+        res.send(err);
+      });
     }else{
       //search modules
       new sql.ConnectionPool(config).connect().then(pool=>{
-          return pool.request().query(`select distinct BookUnit from BookIndex where BookName='${BookName}'`)
+          return pool.request().query(`select distinct BookUnit from BookIndex where BookName='${BookName}'`);
         }).then(result=>{
           let ret = [];
           for( let i=0;i<result.recordset.length;i++){
-            ret.push({Module:result.recordset[i].BookUnit})
+            ret.push({Module:result.recordset[i].BookUnit});
           }
-          res.json(ret)
+          res.json(ret);
         }).catch(err=>{
-          res.send(err)
+          res.send(err);
         })
     }
   }else{
-    res.send(`invalid argumnets,can not find 'BookName'`)
+    res.send(`invalid argumnets,can not find 'BookName'`);
   }
 });
 
@@ -102,18 +139,18 @@ router.get('/unit/', function (req, res) {
   let BookIndex = req.query['BookIndex'];
   if(BookIndex){
       new sql.ConnectionPool(config).connect().then(pool=>{
-          return pool.request().query(`select QuestionID,RawID,UserID from Question where BookIndexID='${BookIndex}'`)
+          return pool.request().query(`select QuestionID,RawID,UserID from Question where BookIndexID='${BookIndex}'`);
         }).then(result=>{
           let ret = [];
           for( let i=0;i<result.recordset.length;i++){
             ret.push({BookIndexID:BookIndex,
             QuestionID:result.recordset[i].QuestionID,
             TopicID:result.recordset[i].RawID,
-            UserID:result.recordset[i].UserID})
+            UserID:result.recordset[i].UserID});
           }
-          res.json(ret)
+          res.json(ret);
         }).catch(err=>{
-          res.send(err)
+          res.send(err);
         })    
   }else{
     res.send(`invalid argumnets,can not find 'BookIndex'`)
@@ -145,21 +182,21 @@ router.get('/topic/', function (req, res) {
   if(QuestionID){
     queryStr = `select state,type,bookindexid,tid,qid,
       topic_body,topic_tag,topic_answer,topic_analysis,topic_image,topic_css
-        from raw_db where rowid='${QuestionID}'`
+        from raw_db where rowid='${QuestionID}'`;
   }else if(tid){
     queryStr = `select state,type,bookindexid,tid,qid,
       topic_body,topic_tag,topic_answer,topic_analysis,topic_image,topic_css
-        from raw_db where tid='${tid}'`    
+        from raw_db where tid='${tid}'`;  
   }else{
-    res.send(`invalid argumnets,can not find 'QuestionID' or 'TopicID' or 'tid'`)
-    return
+    res.send(`invalid argumnets,can not find 'QuestionID' or 'TopicID' or 'tid'`);
+    return;
   }
   new sql.ConnectionPool(config).connect().then(pool=>{
-      return pool.request().query(queryStr)
+      return pool.request().query(queryStr);
     }).then(result=>{
-      res.json(result.recordset[0])
+      res.json(result.recordset[0]);
     }).catch(err=>{
-      res.send(err)
+      res.send(err);
     })      
 });
 
